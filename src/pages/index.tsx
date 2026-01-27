2c74a60180e6bd337987bc178f5270885c054466
Melvin Carvalho <melvincarvalho@gmail.com>
2026-01-27 15:44:01 +0100

    Add Instrumental workbook to landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -85,9 +85,10 @@ function CaseGrid() {
 function WorkbooksSection() {
   const workbooks = [
     { num: 2, name: 'Genitive', path: 'https://grammar.ilearnczech.com/genitive/', status: 'available' },
-    { num: 3, name: 'Dative', path: 'https://grammar.ilearnczech.com/dative/', status: 'new' },
+    { num: 3, name: 'Dative', path: 'https://grammar.ilearnczech.com/dative/', status: 'available' },
     { num: 4, name: 'Accusative', path: 'https://grammar.ilearnczech.com/accusative/', status: 'available' },
     { num: 6, name: 'Locative', path: 'https://grammar.ilearnczech.com/locative/', status: 'available' },
+    { num: 7, name: 'Instrumental', path: 'https://grammar.ilearnczech.com/instrumental/', status: 'new' },
   ];
   return (
     <section className="workbooks-section">
